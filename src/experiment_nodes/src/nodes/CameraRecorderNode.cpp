68a5b8152385a8f9f80d6145a9615f553a8ac5ca
b3rkin <zeybekogluberkin@metu.edu.tr>
2023-12-17 12:03:57 +0000

    Improved the camera tracker to save frames with a thread. Corrected a typo in a package.yml

--- src/experiment_nodes/src/nodes/CameraRecorderNode.cpp
+++ src/experiment_nodes/src/nodes/CameraRecorderNode.cpp
@@ -10,57 +10,86 @@ CameraRecorderNode::CameraRecorderNode() : Node("camera_recorder")
     publisher_ = this->create_publisher<std_msgs::msg::String>("camera_sensor_data", 10);
     subscription_ = this->create_subscription<std_msgs::msg::String>("sync_signal", 10, std::bind(&CameraRecorderNode::sync_callback, this, std::placeholders::_1));
 
-    this->recordFlag.exchange(true);    
+    this->recordFlag.exchange(false);    
 
+    this->inputVideo.open(0); // open the default camera
     this->inputVideo.set(cv::CAP_PROP_FPS, 30);
-    this->inputVideo.open(2); // open the default camera
-    
+    this->captureDone = false;
     startCameraRecorderThread();
+    startSaveThread();
     std::cout << "CameraRecorderNode started" << std::endl;
 
 }
 
 CameraRecorderNode::~CameraRecorderNode()
 {
-    this->joinCameraRecorderThread();
+    this->saveThread.join(); // wait for the save thread to finish
+    this->recorderThread.join();
 }
 
 void CameraRecorderNode::startCameraRecorderThread(){
         
-
-
         recorderThread = std::thread([this]() {
 
-
-            int frameNumber = 0;
-            std::string folderPath = "./captured_frames/"; // folder where images will be saved
             cv::Mat frame;
-
-            while(!this->recordFlag) {}
-
+            while(!this->recordFlag) {} // Wait for the start signal.
             while(this->recordFlag){
+
                 inputVideo >> frame; // get a new frame from camera
-                if (frame.empty()) break; // check if we succeeded
-                
-                // Construct filename for the frame
-                std::string filename = folderPath + "frame_" + std::to_string(frameNumber++) + ".png";
+                if (frame.empty()) break; // check if we succeeded           
 
-                // Save the frame to a file
-                if (!cv::imwrite(filename, frame)) {
-                    std::cerr << "ERROR: Could not save image" << std::endl;
-                    break;
+                {
+                    std::lock_guard<std::mutex> lock(this->FrameMutex);
+                    this->frameQueue.push(frame.clone());
                 }
+                this->frameQueueCondition.notify_one();
+            }
 
-                // Show the frame in a window
-                cv::imshow("Camera", frame);
-
-                // Wait for 1 ms or until a key is pressed. If 'Esc' key (key code 27) is pressed, break the loop.
-                if (cv::waitKey(1) == 27) break;
-                }
+            {
+                std::lock_guard<std::mutex> lock(this->FrameMutex);
+                this->captureDone = true;
+            }
+            this->frameQueueCondition.notify_one();
 
         });
         
     }
+
+
+void CameraRecorderNode::startSaveThread(){
+        
+
+
+    saveThread = std::thread([this]() {
+        int frameNumber = 0;
+        std::string folderPath = "./captured_frames/"; // folder where images will be saved
+
+        while (true) {
+            std::unique_lock<std::mutex> lock(this->FrameMutex);
+            this->frameQueueCondition.wait(lock, [this] { return !this->frameQueue.empty() || this->captureDone; });
+
+            while (!this->frameQueue.empty()) {
+                cv::Mat frame = this->frameQueue.front();
+                this->frameQueue.pop();
+                lock.unlock();
+
+                std::string filename = folderPath + "frame_" + std::to_string(frameNumber++) + ".png";
+                if (!cv::imwrite(filename, frame)) {
+                    std::cerr << "ERROR: Could not save image " << filename << std::endl;
+                }
+
+                lock.lock();
+            }
+
+            if (this->captureDone && this->frameQueue.empty()) {
+                break;
+            }
+        }
+    });     
+}  
+
+
+
 
 void CameraRecorderNode::sync_callback(const std_msgs::msg::String::SharedPtr msg) {
     if (strcmp(msg->data.c_str(), "start") == 0) {
@@ -69,8 +98,4 @@ void CameraRecorderNode::sync_callback(const std_msgs::msg::String::SharedPtr ms
     if (strcmp(msg->data.c_str(), "stop") == 0) {
         this->recordFlag.exchange(false);
     }
-}
-
-void CameraRecorderNode::joinCameraRecorderThread(){
-    this->recorderThread.join();
 }
--- src/experiment_nodes/src/nodes/CameraRecorderNode.hpp
+++ src/experiment_nodes/src/nodes/CameraRecorderNode.hpp
@@ -11,14 +11,21 @@ class CameraRecorderNode : public rclcpp::Node
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
         rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
         std::thread recorderThread;
+        std::thread saveThread;
         void startCameraRecorderThread();
+        void startSaveThread();
         void sync_callback(const std_msgs::msg::String::SharedPtr msg);
+        std::mutex FrameMutex;
+        std::condition_variable frameQueueCondition;
+        std::queue<cv::Mat> frameQueue;
+        bool captureDone;
 
     public:
         std::atomic<bool> recordFlag; 
         cv::VideoCapture inputVideo;
         CameraRecorderNode();
         ~CameraRecorderNode();
-        void joinCameraRecorderThread();
 
 };
+
+
